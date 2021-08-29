999cef48437da12390fe0456afb9a49b3a6409a9
epitchi <thienvanlea1@gmail.com>
2021-08-29 17:02:40 +0700

    Implemeting search

--- src/routes/news.js
+++ src/routes/news.js
@@ -45,4 +45,27 @@ newsRouter.get('/:id', async (req, res) => {
   }
 })
 
+newsRouter.post('', async (req, res) => {
+  let search = req.body.search
+
+
+  try {
+    const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts?search=${search}`)
+    res.render('newsSearch', { articles: newsAPI.data })
+  } catch (err) {
+    if (err.response) {
+      res.render('newsSearch', { articles: null })
+      console.log(err.response.data)
+      console.log(err.response.status)
+      console.log(err.response.header)
+    } else if (err.request) {
+      res.render('newsSearch', { articles: null })
+      console.log(err.request)
+    } else {
+      res.render('newsSearch', { articles: null })
+      console.error('Error', err.message)
+    }
+  }
+})
+
 module.exports = newsRouter
